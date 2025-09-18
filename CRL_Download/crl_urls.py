66c0224bf397da7bb5308c7d302d459a567c498a
Carlitos Perez <charlieperez.cp@outlook.com>
2025-09-17 21:21:29 -0700

    Added crl list file.

---
+++ CRL_Download/crl_urls.py
@@ -0,0 +1,8 @@
+crl_urls = [
+    "https://path_to_file.crl",
+    "https://path_to_file.crl2",
+    "https://path_to_file.crl3",
+    "https://path_to_file.crl4",
+    "https://path_to_crl_bundle/getcrlzip?ALL+CRL+ZIP"
+
+]
